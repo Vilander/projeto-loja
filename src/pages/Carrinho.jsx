f6352cbbc14839e83343d462ef2c9ee40b4bcf68
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-08-04 20:34:03 -0300

    feat: criação de estruturas e codigos iniciais

---
+++ src/pages/Carrinho.jsx
@@ -0,0 +1,9 @@
+export default function Carrinho() {
+    return (
+      <main>
+        <h2>Carrinho de Compras</h2>
+        <p>Ainda não há produtos no carrinho.</p>
+      </main>
+    )
+  }
+  
