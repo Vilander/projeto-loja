5549f9394e04934122bb42b385844cbb841952d3
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-08-04 22:04:04 -0300

    style: alteração em estilo pagina produtos

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -3,13 +3,13 @@
 export default function Produtos({ produtos, adicionarAoCarrinho }) {
   return (
     <main>
-      <h2>Produtos</h2>
-      <ul>
+      <h2 className="prod-h2">Produtos</h2>
+      <ul className="prod-ul">
         {produtos.map(produto => (
-          <li key={produto.id} style={{ marginBottom: '1rem' }}>
+          <li className='prod-li' key={produto.id} style={{ marginBottom: '1rem' }}>
             {produto.nome} - R$ {produto.preco}
             <br />
-            <button onClick={() => adicionarAoCarrinho(produto)}>
+            <button className="prod-btn" onClick={() => adicionarAoCarrinho(produto)}>
               Adicionar ao Carrinho
             </button>
           </li>
